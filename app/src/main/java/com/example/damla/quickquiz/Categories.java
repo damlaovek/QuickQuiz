6c115ea3371fcd58556f66cb6e185bb600220ee1
damla1409 <damlaovek94@gmail.com>
2017-02-28 20:53:09 +0300

    damla

---
+++ app/src/main/java/com/example/damla/quickquiz/Categories.java
@@ -0,0 +1,150 @@
+package com.example.damla.quickquiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.id;
+
+/**
+ * Created by Damla on 28.2.2017.
+ */
+
+public class Categories {
+
+    public static Categories ourInstance = new Categories();
+
+    public static Categories getInstance(){return ourInstance;}
+
+    private Categories(){}
+
+    public Question getQuestion(String questionID) {
+        String questionText="";
+        int score=0;
+        List<String> choices=new ArrayList<String>();
+        String correctAnswer="";
+        switch (questionID) {
+            case "science100":
+                questionText = "What does a light-year measure?";
+                score = 100;
+                choices.add("Brightness");
+                choices.add("Time");
+                choices.add("Distance");
+                choices.add("Weight");
+                correctAnswer = "b";
+            case "science200":
+                questionText = "Which of these terms is defined as the study of how the positions of stars and planets can influence human behavior?";
+                score = 200;
+                choices.add("Astrology");
+                choices.add("Alchemy");
+                choices.add("Astronomy");
+                choices.add("Meteorology");
+                correctAnswer = "a";
+            case "science300":
+                questionText = "Which of these elements is needed to make nuclear energy and nuclear weapons?";
+                score = 300;
+                choices.add("Sodium chloride");
+                choices.add("Uranium");
+                choices.add("Nitrogen");
+                choices.add("Carbon dioxide");
+                correctAnswer = "b";
+            case "science400":
+                questionText = "The loudness of a sound is determined by what property of a sound wave?";
+                score = 400;
+                choices.add("Frequency");
+                choices.add("Wavelength");
+                choices.add("Velocity");
+                choices.add("Amplitude");
+                correctAnswer = "d";
+            case "science500":
+                questionText = "Which kind of waves are used to make and receive cellphone calls?";
+                score = 500;
+                choices.add("Radio waves");
+                choices.add("Visible light waves");
+                choices.add("Sound waves");
+                choices.add("Gravity waves");
+                correctAnswer = "a";
+            case "sport100":
+                questionText = "With which sport is the 'Choudhury Trophy' associated?";
+                score = 100;
+                choices.add("Golf");
+                choices.add("Circumnavigation of the earth by car");
+                choices.add("Formula One racing");
+                choices.add("Buggy-Jumping");
+                correctAnswer = "b";
+            case "sport200":
+                questionText = "Which English Premier League football club supporters traditionally sing the song ‘You'll Never Walk Alone’ before commencement of the match?";
+                score = 200;
+                choices.add("Chelsea FC");
+                choices.add("Manchester United");
+                choices.add("Liverpool FC");
+                choices.add("Leeds United FC");
+                correctAnswer = "c";
+            case "sport300":
+                questionText = "Which 2 male tennis players have the most Wimbledon singles titles, seven each?";
+                score = 300;
+                choices.add("Roger Federer and Andre Agassi");
+                choices.add("Roger Federer and Raphael Nadal");
+                choices.add("Pete Sampras and Andre Agassi");
+                choices.add("Roger Federer and Pete Sampras");
+                correctAnswer = "d";
+            case "sport400":
+                questionText = "Which Australian was selected by the Milwaukee Bucks with the first overall pick in the 2005 National Basketball Association (NBA) Draft?";
+                score = 400;
+                choices.add("Mark Bradtke");
+                choices.add("Andrew Bogut");
+                choices.add("Patrick Mills");
+                choices.add("Luke Schenscher");
+                correctAnswer = "b";
+            case "sport500":
+                questionText = "At 9.58 seconds, Usain Bolt holds the record for the fastest time in the 100 metre sprint, but which sprinter holds the female record with 10.49 seconds?";
+                score = 500;
+                choices.add("Marion Jones");
+                choices.add("Shelly-Ann Fraser-Pryce");
+                choices.add("Florence Griffith-Joyner");
+                choices.add("Carmelita Jeter");
+                correctAnswer = "c";
+            case "art100":
+                questionText = "Early photographers made their images on which of these materials?";
+                score = 100;
+                choices.add("Stone");
+                choices.add("Glass");
+                choices.add("Paper");
+                choices.add("Plastic");
+                correctAnswer = "b";
+            case "art200":
+                questionText = "Which one of these is not a well-known Indian sculptor?";
+                score = 200;
+                choices.add("Ramkinkar Baij");
+                choices.add("Krishna Reddy");
+                choices.add("Henry Moore");
+                choices.add("Kumaradeva");
+                correctAnswer = "c";
+            case "art300":
+                questionText = "What did I.M. Pei design outside the Louvre, in Paris?";
+                score = 300;
+                choices.add("Obelisk");
+                choices.add("Sarcophagus");
+                choices.add("Ziggurat");
+                choices.add("Pyramid");
+                correctAnswer = "d";
+            case "art400":
+                questionText = "Who designed the Vietnam Veterans Memorial?";
+                score = 400;
+                choices.add("Maya Lin");
+                choices.add("Frank Llyod Wright");
+                choices.add("I.M. Pei");
+                choices.add("Frank Gehry");
+                correctAnswer = "a";
+            case "art500":
+                questionText = "What does the Venus of Brassempouy represent?";
+                score = 500;
+                choices.add("A human figure");
+                choices.add("An old man");
+                choices.add("A woman's body");
+                choices.add("An angel");
+                correctAnswer = "c";
+        }
+        Question myQuestion=new Question(questionText, score, choices, correctAnswer);
+        return myQuestion;
+    }
+}
